0e45bcb69a3d63723282e3c007fc12576e7ec2ba
Ben Sokol <bensokol@ku.edu>
2017-08-29 14:47:40 -0500

    Fix whitespace issues in LinkedList.hpp
    Whitespace was showing random tabs and spacing causing mis-aligned
    braces.

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -14,86 +14,86 @@ LinkedList<T>::LinkedList() : m_front(nullptr), m_size(0)
 template <typename T>
 LinkedList<T>::~LinkedList()
 {
-	while(!isEmpty())
-	{
-		removeFront();
-	}
+  while(!isEmpty())
+  {
+    removeFront();
+  }
 }
 
 template <typename T>
 bool LinkedList<T>::isEmpty() const
 {
-	return(m_size == 0);
+  return(m_size == 0);
 }
 
 template <typename T>
 int LinkedList<T>::size() const
 {
-	return( m_size );
+  return( m_size );
 }
 
 template <typename T>
 bool LinkedList<T>::search(T value) const
 {
   Node<T>* temp = m_front;
-	bool isFound = false;
+  bool isFound = false;
   while( temp != nullptr )
   {
-		if( temp -> getValue() == value )
+    if( temp -> getValue() == value )
     {
-			isFound = true;
-		}
-		temp = temp->getNext();
-	}
-	return( isFound );
+      isFound = true;
+    }
+    temp = temp->getNext();
+  }
+  return( isFound );
 }
 
 template <typename T>
 std::vector<T> LinkedList<T>::toVector() const
 {
-	std::vector<T> vec;
-	Node<T>* temp = m_front;
+  std::vector<T> vec;
+  Node<T>* temp = m_front;
 
-	while( temp != nullptr )
-	{
-		vec.push_back(temp->getValue());
-		temp = temp->getNext();
-	}
+  while( temp != nullptr )
+  {
+    vec.push_back(temp->getValue());
+    temp = temp->getNext();
+  }
 
-	return(vec);
+  return(vec);
 
 }
 
 template <typename T>
 void LinkedList<T>::addBack(T value)
 {
-	Node<T>* temp = nullptr;
+  Node<T>* temp = nullptr;
 
-	if(isEmpty())
-	{
-		m_front = new Node<T>(value);
-	}
-	else
-	{
-		temp = m_front;
-		while(temp->getNext() != nullptr)
-		{
-			temp = temp->getNext();
-		}
+  if(isEmpty())
+  {
+    m_front = new Node<T>(value);
+  }
+  else
+  {
+    temp = m_front;
+    while(temp->getNext() != nullptr)
+    {
+      temp = temp->getNext();
+    }
 
-		temp->setNext( new Node<T>(value) );
-	}
+    temp->setNext( new Node<T>(value) );
+  }
 
-	m_size++;
+  m_size++;
 }
 
 template <typename T>
 void LinkedList<T>::addFront(T value)
 {
-	Node<T>* temp = m_front;
-	m_front = new Node<T>(value);
-	m_front->setNext( temp );
-	m_size++;
+  Node<T>* temp = m_front;
+  m_front = new Node<T>(value);
+  m_front->setNext( temp );
+  m_size++;
 }
 
 template <typename T>
@@ -101,42 +101,42 @@ bool LinkedList<T>::removeBack()
 {
   if( isEmpty() )
   {
-		return false;
-	}
-	Node<T>* lastNode = m_front;
-	Node<T>* secondtoLast = nullptr;
-	while( lastNode->getNext() != nullptr )
+    return false;
+  }
+  Node<T>* lastNode = m_front;
+  Node<T>* secondtoLast = nullptr;
+  while( lastNode->getNext() != nullptr )
   {
-		secondtoLast = lastNode;
-		lastNode = lastNode->getNext();
-	}
-	if( lastNode == m_front )
+    secondtoLast = lastNode;
+    lastNode = lastNode->getNext();
+  }
+  if( lastNode == m_front )
   {
-		m_front = nullptr;
-	}
-	if( lastNode != nullptr )
+    m_front = nullptr;
+  }
+  if( lastNode != nullptr )
   {
-		delete lastNode;
-		m_size--;
-	}
-	secondtoLast->setNext( nullptr );
-	return true;
+    delete lastNode;
+    m_size--;
+  }
+  secondtoLast->setNext( nullptr );
+  return true;
 }
 
 template <typename T>
 bool LinkedList<T>::removeFront()
 {
-	Node<T>* temp = nullptr;
-	bool isRemoved = false;
+  Node<T>* temp = nullptr;
+  bool isRemoved = false;
 
-	if(!isEmpty())
-	{
-		temp = m_front;
-		m_front = temp->getNext();
-		delete temp;
-		m_size--;
-		isRemoved = true;
-	}
+  if(!isEmpty())
+  {
+    temp = m_front;
+    m_front = temp->getNext();
+    delete temp;
+    m_size--;
+    isRemoved = true;
+  }
 
-	return(isRemoved);
+  return(isRemoved);
 }
